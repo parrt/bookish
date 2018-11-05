4022e011fd328d92161fb070803e8907b5d67825
parrt <parrt@cs.usfca.edu>
2018-11-05 14:27:24 -0800

    print out chap, sec numbers in defs

--- src/us/parr/bookish/model/ContainerWithTitle.java
+++ src/us/parr/bookish/model/ContainerWithTitle.java
@@ -10,12 +10,9 @@ public abstract class ContainerWithTitle extends OutputModelObject {
 	@ModelElement
 	public List<ContainerWithTitle> subcontainers;
 
-	public ContainerWithTitle parent; // who contains us?
 	public String title;
 
 	public EntityDef def;
-
-	public int sectionNumber;
 
 	public ContainerWithTitle(EntityDef def, String title, OutputModelObject content) {
 		this.def = def;
